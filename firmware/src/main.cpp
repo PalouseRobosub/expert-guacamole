4ea656eb2d584515ea87d6ea43fb70694931678d
James Irwin <irwineffect@gmail.com>
2017-03-25 19:35:11 -0700

    more work to firmware skeleton

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -2,10 +2,12 @@
 #include "sensors.h"
 
 Sensors sensors;
+Switch cancel_button;
 
 void setup()
 {
     sensors.init();
+    cancel_button.init(3, Switch::Polarity::ACTIVE_HIGH);
 }
 
 void loop()
@@ -13,7 +15,22 @@ void loop()
     bool opened = sensors.mailbox_opened();
     if(opened == true)
     {
-        //send message
+        bool button_pressed = false;
+        //wait for up to 30 seconds
+        for (uint8_t i = 0; i < 30*50; ++i)
+        {
+            if (cancel_button.read() == true)
+            {
+                button_pressed = true;
+                break;
+            }
+
+            delay(50);
+        }
+        if(button_pressed != true)
+        {
+            //send message
+        }
     }
 
     delay(1000);

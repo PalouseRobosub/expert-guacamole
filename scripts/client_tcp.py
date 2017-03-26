469dea7ccd4f064fff1aa3c4fe3f14c91a3ea15d
James Irwin <irwineffect@gmail.com>
2017-03-26 09:19:11 -0700

    added fake tcp client script

--- scripts/client_tcp.py
+++ scripts/client_tcp.py
@@ -1,22 +1,18 @@
 #!/usr/bin/python
-import zmq
+import socket
 import argparse
-import time
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-p", "--port", required=True)
+    parser.add_argument("-p", "--port", required=True, type=int)
     parser.add_argument("address")
     args = parser.parse_args()
 
-    context = zmq.Context()
     print "Connecting to server..."
-    socket = context.socket(zmq.PAIR)
-    socket.connect("tcp://{}:{}".format(args.address, args.port))
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((args.address, args.port))
 
     print "Sending message"
-    socket.send("ryan sucks")
-    time.sleep(.1)
+    sock.sendall("ryan sucks")
     print "Done!"
-    socket.close()
-
+    sock.close()
